7bc3b22e06b4d1c21b95ecbb9dd3c47349aabedf
andre.diogenes <andre.diogenes@meta.com.br>
2022-06-24 15:22:28 -0300

    começando request ao banco

--- app/src/main/java/alura/com/livrariaapp/DAO/DAO.java
+++ app/src/main/java/alura/com/livrariaapp/DAO/DAO.java
@@ -241,21 +241,38 @@ public class DAO extends SQLiteOpenHelper {
         c.close();
         return id;
     }
-    /*
+
     //Funcao que retorna uma lista com os usuários
     public List<Usuario> consultaUsuarios(){
-        SQLiteDatabase db = getWritableDatabase();
-        String sqli_busca_usuario = "SELECT * FROM USUARIO";
+        SQLiteDatabase db = getReadableDatabase();
 
-        Cursor c = db.rawQuery(sqli_busca_usuario, null);
+// Define a projection that specifies which columns from the database
+// you will actually use after this query.
+        //https://developer.android.com/training/data-storage/sqlite?hl=pt-br#java
+        //https://www.youtube.com/watch?v=hRptcOy1g2M
+        String[] projection = {
+                BaseColumns._ID,
+                FeedEntry.COLUMN_NAME_TITLE,
+                FeedEntry.COLUMN_NAME_SUBTITLE
+        };
 
-        List<Usuario> usuarios = new ArrayList<Usuario>();
+// Filter results WHERE "title" = 'My Title'
+        String selection = FeedEntry.COLUMN_NAME_TITLE + " = ?";
+        String[] selectionArgs = { "My Title" };
 
-        while(c.moveToNext()){
-            Usuario auxiliar = new Usuario();
-            c.getColumnIndex("USUARIO_ADM");
+// How you want the results sorted in the resulting Cursor
+        String sortOrder =
+                FeedEntry.COLUMN_NAME_SUBTITLE + " DESC";
 
-            auxiliar.setUsuario_adm();
+        Cursor cursor = db.query(
+                FeedEntry.TABLE_NAME,   // The table to query
+                projection,             // The array of columns to return (pass null to get all)
+                selection,              // The columns for the WHERE clause
+                selectionArgs,          // The values for the WHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                sortOrder               // The sort order
+        );
         }
-    }*/
+    }
 }
